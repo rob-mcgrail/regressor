edf452d99310684e443fabe7dce7778043714c54
Rob McGrail <rob.mcgrail@gmail.com>
2019-09-18 13:18:27 +1200

    Update controller_generator.rb

--- lib/generators/regressor/controller_generator.rb
+++ lib/generators/regressor/controller_generator.rb
@@ -8,7 +8,7 @@ module Regressor
       Rails.application.eager_load!
       ApplicationController.descendants.map(&:name).reject { |x| Regressor.configuration.excluded_controllers.include? x }.each do |controller|
         @controller = Regressor::Controller::Regression.new(controller)
-        create_file "#{Regressor.configuration.regression_controller_path}/#{controller.underscore}_spec.rb", ERB.new(File.new(File.expand_path('../../templates/controller/controller_spec_template.erb', File.dirname(__FILE__))).read).result(binding)
+        create_file "#{Regressor.configuration.regression_controller_path}/#{controller.underscore}_routing_spec.rb", ERB.new(File.new(File.expand_path('../../templates/controller/controller_spec_template.erb', File.dirname(__FILE__))).read).result(binding)
       end
     end
 
